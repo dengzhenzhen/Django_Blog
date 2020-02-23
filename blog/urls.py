ee0a85297dc4cf5f6a878fed4bf2ba589b00a41f
dengzhenzhen <zhenzhendeng2012@gmail.com>
2020-02-24 00:01:45 +0800

    Rewrite frountend with Vue. Changed some of responses. 	modified:   blog/templates/blog/index.html 	modified:   blog/templates/blog/post.html 	modified:   blog/urls.py 	modified:   blog/views.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index',),
     path('test', views.test, name='test'),
     path('about', views.about_page, name='about'),
-    path('blog/<int:post_id>', views.post_page, name='post_page')
+    path('blog/<int:post_id>', views.post_page, name='post_page'),
+    path('resume', views.resume, name='resume')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseNotFound
+from django.http import HttpResponse, Http404, HttpResponseNotFound, JsonResponse
 from .models import blog_post, blog_information
+import json
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html', context=blog_post().get_index_data())
+    # return render(request, 'blog/index.html', context=blog_post().get_index_data())
+    # When using Vue as frontend, DO NOT use render, because there are conflicts between their programmers.
+    with open("D:/Repositories/Django_Blog/blog/templates/blog/index.html", 'rb') as fp:
+        tmp = fp.read()
+    return HttpResponse(tmp)
 
 def post_page(request, post_id):
     data = blog_post().get_post_data_by_postid(post_id)
-    return render(request, 'blog/post.html', context=data) if data else HttpResponseNotFound()
+    # return render(request, 'blog/post.html', context=data) if data else HttpResponseNotFound()
+    return JsonResponse(data) if data else HttpResponseNotFound()
 
 
 def about_page(request):
         return render(request, 'blog/about.html', context=blog_information().get_blog_information())
 
 def test(request):
+    data = blog_post().get_index_data()
+    # return render(request, 'blog/test.html')
+    # return HttpResponse(json.dumps(data),content_type="application/json")
+    return JsonResponse(data) if data else HttpResponseNotFound()
 
-    return render(request, 'blog/test.html')
+def resume(request):
+    path = "D:/Repositories/Resume/resume.md"
+    with open(path, mode='r', errors='ignore', encoding='utf-8') as fp:
+        data = {'data':{"text":fp.read()}}
+    return render(request, 'blog/resume.html', context=data)
