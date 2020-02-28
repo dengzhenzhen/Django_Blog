261123aa682cc45f7d1528fd3311a768480768a3
dengzhenzhen <zhenzhendeng2012@gmail.com>
2020-02-29 00:42:13 +0800

    use query params instead 	modified:   blog/templates/blog/index.html 	modified:   blog/urls.py 	modified:   blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseNotFound, JsonResponse
-from .models import blog_post, blog_information
 import json
+
+from django.http import (Http404, HttpResponse, HttpResponseNotFound,
+                         JsonResponse)
+from django.shortcuts import render
+
+from .models import blog_information, blog_post
+
 # Create your views here.
 
 def index(request):
     # return render(request, 'blog/index.html', context=blog_post().get_index_data())
     # When using Vue as frontend, DO NOT use render, because there are conflicts between their programmers.
+
+    params = dict(request.GET)
+    print(params)
+    if params.get('content'):
+
+        if 'post_list' in params.get('content'):
+            data = blog_post().get_index_data()
+
+        elif 'post' in  params.get('content'):
+            data = blog_post().get_post_data_by_postid(params.get('post_id')[0]) if params.get('post_id') else None
+        
+        return JsonResponse(data) if data else HttpResponseNotFound()
+    
     with open("D:/Repositories/Django_Blog/blog/templates/blog/index.html", 'rb') as fp:
-        tmp = fp.read()
-    return HttpResponse(tmp)
+        index_page = fp.read()
+    return HttpResponse(index_page)
 
-def post_page(request, post_id):
-    data = blog_post().get_post_data_by_postid(post_id)
-    # return render(request, 'blog/post.html', context=data) if data else HttpResponseNotFound()
-    return JsonResponse(data) if data else HttpResponseNotFound()
+# def post_page(request, post_id):
+#     data = blog_post().get_post_data_by_postid(post_id)
+#     # return render(request, 'blog/post.html', context=data) if data else HttpResponseNotFound()
+#     return JsonResponse(data) if data else HttpResponseNotFound()
 
 
-def about_page(request):
-        return render(request, 'blog/about.html', context=blog_information().get_blog_information())
+# def about_page(request):
+#         return render(request, 'blog/about.html', context=blog_information().get_blog_information())
 
 def test(request):
     data = blog_post().get_index_data()
